b43c6c63990991a3c44955113e386760962f9545
ebubekir000 <ebkrofficial@gmail.com>
2023-11-26 19:31:01 +0300

    proje Güncellendi. 03.12.2023

---
+++ src/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+export const Home = () => {
+    return(
+        <div>
+            <div>
+                bir oyun seç
+            </div>
+
+            <Link to={"/adamasmaca"}>oyna</Link>
+            <Link to={"/oyuniki"}>oyna 2</Link>
+        </div>
+    );
+}
